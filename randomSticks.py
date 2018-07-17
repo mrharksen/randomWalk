f41a923570c83c06073fa87f8d8c1afad8ca1327
mrharksen <matthias.harksen@gmail.com>
2018-07-17 10:50:14 +0000

    randomSticks added
    Added the program made by AES, randomSticks

---
+++ randomSticks.py
@@ -0,0 +1,216 @@
+import numpy as np
+import random
+from scipy.special import zeta
+import matplotlib.pyplot as plt
+
+
+def randomSticks(n = 100, p0 = 0.5, alpha = 1.5, drawWalk = True, drawSticks = True):
+	c = findConstant(p0,alpha)
+	p1 = findp1(p0,alpha,c)
+	dist = Distribution(p0, p1, alpha, c)
+	randomWalk = [0 for i in xrange(n)]
+	length = n
+	cnt = 0
+	index = 0
+	while(cnt < n):
+		index += 1
+		step = getStepSize(dist)
+		if( step >= 0):
+			cnt += 1
+		if(index == length):
+			length *= 2
+			temp = [0 for i in xrange(length)]
+			for i in xrange(length/2):
+				temp[i] = randomWalk[i]
+			del randomWalk
+			randomWalk = temp
+		randomWalk[index] = randomWalk[index-1]+step
+	randomWalk = randomWalk[:index+1]
+
+	if drawWalk:
+		plt.plot(range(len(randomWalk)), randomWalk)
+		if drawSticks:
+			tops, bottoms, xjumps = jumps(randomWalk, n)
+			for i in range(len(xjumps)):
+				x = (xjumps[i]+1, xjumps[i]+1)
+				y = (bottoms[i], tops[i]) 
+				plt.plot(x, y,  'C1')
+			tops, bottoms, xjumps = removeSticks(tops, bottoms, xjumps, n)
+			for i in range(len(xjumps)):
+				x = (xjumps[i]+1, xjumps[i]+1)
+				y = (bottoms[i], tops[i]) 
+				plt.plot(x, y,  'C4')
+		plt.show()
+
+	return randomWalk
+
+
+def jumps(randomWalk, n):
+	tops = [0 for i in xrange(n)]
+	bottoms = [0 for i in xrange(n)]
+	xjumps = [0 for i in xrange(n)]
+	cnt = 0;
+	for i in xrange(len(randomWalk)-1):
+		if randomWalk[i+1] >= randomWalk[i]:
+			bottoms[cnt] = randomWalk[i]
+			tops[cnt] = randomWalk[i+1]
+			xjumps[cnt] = i
+			cnt = cnt + 1
+	return tops, bottoms, xjumps
+
+'''
+def jumpTops(randomWalk, n):
+	tops = [0 for i in xrange(n)]
+	cnt = 0;
+	for i in xrange(len(randomWalk)-1):
+		if randomWalk[i+1] >= randomWalk[i]:
+			tops[cnt] = randomWalk[i+1]
+			cnt = cnt + 1
+	return tops
+
+
+def jumpBottoms(randomWalk):
+	bottoms = [0 for i in xrange(n)]
+	cnt = 0;
+	for i in xrange(len(randomWalk)-1):
+		if randomWalk[i+1] >= randomWalk[i]:
+			bottoms[cnt] = randomWalk[i]
+			cnt = cnt + 1
+	return bottoms
+'''
+
+def rightShadow(tops, bottoms, i, n):
+	TOP = tops[i]
+	BOTTOM = bottoms[i]
+	shadow = [0 for j in xrange(n)]
+	count = 0
+	for j in xrange(i+1,i+n):
+		j = j % n
+		if TOP < BOTTOM:
+			break
+		if bottoms[j] >= BOTTOM and tops[j] <= TOP:
+			shadow[j] = 1
+		else:
+			if bottoms[j] < BOTTOM and tops[j] > TOP:
+				break
+			if bottoms[j] < BOTTOM and tops[j] > BOTTOM:
+				BOTTOM = tops[j]
+			if bottoms[j] < TOP and tops[j] > TOP:
+				TOP = bottoms[j]
+	return shadow
+
+
+def leftShadow(tops, bottoms, i, n):
+	TOP = tops[i]
+	BOTTOM = bottoms[i]
+	shadow = [0 for j in xrange(n)]
+	count = 0
+	for j in xrange(i-1,i-n+2,-1):
+		j = j % n
+		if TOP < BOTTOM:
+			break
+		if bottoms[j] >= BOTTOM and tops[j] <= TOP:
+			shadow[j] = 1
+		else:
+			if bottoms[j] < BOTTOM and tops[j] > TOP:
+				break
+			if bottoms[j] < BOTTOM and tops[j] > BOTTOM:
+				BOTTOM = tops[j]
+			if bottoms[j] < TOP and tops[j] > TOP:
+				TOP = bottoms[j]
+	return shadow
+
+
+def removeSticks(tops, bottoms, xjumps, n):
+	sticks = [1 for i in xrange(n)]
+	for i in xrange(n):
+		if sticks[i] == 1:
+			skuggar = leftShadow(tops, bottoms, i, n)
+			for j in xrange(n):
+				j= j % n
+				if skuggar[j] == 1:
+					sticks[j] = 0
+	noRemaining = sum(sticks)
+	newTops = [0 for i in xrange(noRemaining)]
+	newBottoms = [0 for i in xrange(noRemaining)]
+	newXjumps = [0 for i in xrange(noRemaining)]
+	cnt = 0
+	for i in xrange(n):
+		if sticks[i] == 1:
+			newTops[cnt] = tops[i]
+			newBottoms[cnt] = bottoms[i]
+			newXjumps[cnt] = xjumps[i]
+			cnt = cnt + 1
+	return newTops, newBottoms, newXjumps
+
+
+def whoRemove(tops, bottoms, xjumps, n):
+	sticks = [1 for i in xrange(n)]
+	for i in xrange(n):
+		if sticks[i] == 1:
+			skuggar = leftShadow(tops, bottoms, i, n)
+			for j in xrange(n):
+				j= j % n
+				if skuggar[j] == 1:
+					sticks[j] = 0
+	return sticks
+'''
+    Finds the constant c by the conditions above.
+'''
+
+def findConstant(p0, alpha):
+	return p0/(zeta(alpha)-zeta(alpha+1))
+
+'''
+    Finds the probability p1 by the conditions above.
+'''
+def findp1(p0, alpha, c):
+	return 1+c-c*zeta(alpha)
+
+'''
+    Generates the distribution function of the corresponding probabilities
+	p0, p1, ... such that sum pi > 0.99999999 (which can be altered).
+'''
+def Distribution(p0, p1, alpha, c):
+	invDist = []
+	invDist.append(p0)
+	invDist.append(p0+p1)
+	cumsum = p0+p1
+	j = 1
+	while(0.99999999 > cumsum):
+		cumsum += c*np.power((j+1),-(alpha+1))
+		invDist.append(cumsum)
+		j += 1
+	return invDist
+
+'''
+    Finds the size of the step which the random walk takes next.
+'''
+def getStepSize(dist):
+	r = np.random.uniform()
+	for x in dist:
+		if r < x:
+			return dist.index(x)-1
+	return len(dist)-1
+
+def main():
+	n = 100
+	p0 = 0.5
+	alpha = 1.5
+	rnd = randomSticks(n,p0,alpha)
+	tops, bottoms, xjumps = jumps(rnd, n)
+	#tops = jumpTops(rnd)
+	#bottoms = jumpBottoms(rnd)
+'''
+	print str(rnd)
+
+	for i in xrange(n):
+		print str(bottoms[i])+" -> "+str(tops[i])
+
+	for i in xrange(n):
+		print inShadow(tops, bottoms, i, n)
+
+	print "og svo kemur: "+str(removeSticks(tops, bottoms, n))
+'''
+if __name__ == "__main__":
+	main()
--- statWalk.py
+++ statWalk.py
@@ -3,9 +3,6 @@ import scipy.stats as stats
 import pylab as pl
 
 a40b = np.loadtxt('alpha_40n100.txt')
-a40b.sort()
-fita40b = stats.norm.pdf(a40b, np.mean(a40b), np.std(a40b))
-pl.plot(a40b,fita40b,'-o')
 pl.hist(a40b,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.4$')
 pl.ylabel("Probability")
@@ -18,9 +15,6 @@ print 'mean = ' + str(np.mean(a40b))
 print 'samplesize = ' + str(len(a40b)) + '\n'
 
 a40 = np.loadtxt('alpha_40.txt')
-a40.sort()
-fita40 = stats.norm.pdf(a40, np.mean(a40), np.std(a40))
-pl.plot(a40,fita40,'-o')
 pl.hist(a40,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.4$')
 pl.ylabel("Probability")
@@ -33,9 +27,6 @@ print 'mean = ' + str(np.mean(a40))
 print 'samplesize = ' + str(len(a40)) + '\n'
 
 a45b = np.loadtxt('alpha_45n100.txt')
-a45b.sort()
-fita45b = stats.norm.pdf(a45b, np.mean(a45b), np.std(a45b))
-pl.plot(a45b,fita45b,'-o')
 pl.hist(a45b,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.45$')
 pl.ylabel("Probability")
@@ -48,9 +39,6 @@ print 'mean = ' + str(np.mean(a45b))
 print 'samplesize = ' + str(len(a45b)) + '\n'
 
 a45 = np.loadtxt('alpha_45.txt')
-a45.sort()
-fita45 = stats.norm.pdf(a45, np.mean(a45), np.std(a45))
-pl.plot(a45,fita45,'-o')
 pl.hist(a45,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.45$')
 pl.ylabel("Probability")
@@ -63,9 +51,6 @@ print 'mean = ' + str(np.mean(a45))
 print 'samplesize = ' + str(len(a45)) + '\n'
 
 a50b = np.loadtxt('alpha_50n100.txt')
-a50b.sort()
-fita50b = stats.norm.pdf(a50b, np.mean(a50b), np.std(a50b))
-pl.plot(a50b,fita50b,'-o')
 pl.hist(a50b,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.5$')
 pl.ylabel("Probability")
@@ -78,9 +63,6 @@ print 'mean = ' + str(np.mean(a50b))
 print 'samplesize = ' + str(len(a50b)) + '\n'
 
 a50d = np.loadtxt('alpha_50n200.txt')
-a50d.sort()
-fita50d = stats.norm.pdf(a50d, np.mean(a50d), np.std(a50d))
-pl.plot(a50d,fita50d,'-o')
 pl.hist(a50d,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.5$')
 pl.ylabel("Probability")
@@ -93,9 +75,6 @@ print 'mean = ' + str(np.mean(a50d))
 print 'samplesize = ' + str(len(a50d)) + '\n'
 
 a50e = np.loadtxt('alpha_50n300.txt')
-a50e.sort()
-fita50e = stats.norm.pdf(a50e, np.mean(a50e), np.std(a50e))
-pl.plot(a50e,fita50e,'-o')
 pl.hist(a50e,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.5$')
 pl.ylabel("Probability")
@@ -108,9 +87,6 @@ print 'mean = ' + str(np.mean(a50e))
 print 'samplesize = ' + str(len(a50e)) + '\n'
 
 a50 = np.loadtxt('alpha_50.txt')
-a50.sort()
-fita50 = stats.norm.pdf(a50, np.mean(a50), np.std(a50))
-pl.plot(a50,fita50,'-o')
 pl.hist(a50,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.5$')
 pl.ylabel("Probability")
@@ -123,9 +99,6 @@ print 'mean = ' + str(np.mean(a50))
 print 'samplesize = ' + str(len(a50)) + '\n'
 
 a50c = np.loadtxt('alpha_50n10000.txt')
-a50c.sort()
-fita50c = stats.norm.pdf(a50c, np.mean(a50c), np.std(a50c))
-pl.plot(a50c,fita50c,'-o')
 pl.hist(a50c,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.5$')
 pl.ylabel("Probability")
@@ -138,9 +111,6 @@ print 'mean = ' + str(np.mean(a50c))
 print 'samplesize = ' + str(len(a50c)) + '\n'
 
 a55b = np.loadtxt('alpha_55n100.txt')
-a55b.sort()
-fita55b = stats.norm.pdf(a55b, np.mean(a55b), np.std(a55b))
-pl.plot(a55b,fita55b,'-o')
 pl.hist(a55b,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.55$')
 pl.ylabel("Probability")
@@ -153,9 +123,6 @@ print 'mean = ' + str(np.mean(a55b))
 print 'samplesize = ' + str(len(a55b)) + '\n'
 
 a55 = np.loadtxt('alpha_55.txt')
-a55.sort()
-fita55 = stats.norm.pdf(a55, np.mean(a55), np.std(a55))
-pl.plot(a55,fita55,'-o')
 pl.hist(a55,normed=True, bins=15)
 pl.title('For ' + r'$ \alpha = 1.55$')
 pl.ylabel("Probability")
@@ -168,9 +135,6 @@ print 'mean = ' + str(np.mean(a55))
 print 'samplesize = ' + str(len(a55)) + '\n'
 
 a60b = np.loadtxt('alpha_60n100.txt')
-a60b.sort()
-fita60b = stats.norm.pdf(a60b, np.mean(a60b), np.std(a60b))
-pl.plot(a60b,fita60b,'-o')
 pl.hist(a60b,normed=True,bins=15)
 pl.title('For ' + r'$ \alpha = 1.6$')
 pl.ylabel("Probability")
@@ -183,9 +147,6 @@ print 'mean = ' + str(np.mean(a60b))
 print 'samplesize = ' + str(len(a60b)) + '\n'
 
 a60 = np.loadtxt('alpha_60.txt')
-a60.sort()
-fita60 = stats.norm.pdf(a60, np.mean(a60), np.std(a60))
-pl.plot(a60,fita60,'-o')
 pl.hist(a60,normed=True,bins=15)
 pl.title('For ' + r'$ \alpha = 1.6$')
 pl.ylabel("Probability")
